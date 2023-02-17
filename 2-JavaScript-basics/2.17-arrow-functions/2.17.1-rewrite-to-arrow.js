cc7af6283875a1bac07fd6a77ac455a1c76d4e22
Alexandr Panteleev <88164077+Gabageba@users.noreply.github.com>
2023-02-17 22:40:18 +0700

    2.17 complete

---
+++ 2-JavaScript-basics/2.17-arrow-functions/2.17.1-rewrite-to-arrow.js
@@ -0,0 +1,9 @@
+const ask = (question, yes, no) => {
+  confirm(question) ? yes() : no()
+}
+
+ask(
+  'Вы согласны?',
+  () => console.log('Вы согласились'),
+  () => console.log('Вы отменили выполнение'),
+)
